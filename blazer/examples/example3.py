1db0678f350ef02308260153e73a966ebea0a72e
Darren Govoni <darren@rapidquest.ai>
2022-02-12 10:49:12 -0500

    Updated map/reduce example

--- blazer/examples/example3.py
+++ blazer/examples/example3.py
@@ -1,14 +1,14 @@
 import blazer
 from blazer.hpc.mpi import map, reduce
 
-def mult(x,y):
-    return x * y
+def sqr(x):
+    return x * x
 
 def add(x, y=0):
     return x+y
 
 with blazer.begin():
-    result = map(mult, [(1,1), (2,2), (3,3), (4,4)])
+    result = map(sqr, [1, 2, 3, 4])
 
     blazer.print(result)
     result = reduce(add, result)
