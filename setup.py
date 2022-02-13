6ada8ba1621e4e7c0b92946864edb92a1bddd432
Darren Govoni <darren@rapidquest.ai>
2022-02-13 17:22:10 -0500

    Updates

--- setup.py
+++ setup.py
@@ -120,10 +120,14 @@ setup(
     author=about['__author__'],
     author_email=about['__author_email__'],
     url=about['__url__'],
-    packages=['blazer', 'blazer.hpc', 'blazer.hpc.local', 'blazer.hpc.mpi'],
+    packages=['blazer', 'blazer.hpc', 'blazer.hpc.mpi'],
     include_package_data=True,
     python_requires=">=3.8.*",
     install_requires=[
+        'pydash==5.1.0',
+        'numpy==1.22.2',
+        'mpi4py==3.1.3',
+        'dill==0.3.4'
     ],
     license=about['__license__'],
     zip_safe=False,
