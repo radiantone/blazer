698d2f9903c1023fc15385a7428ec4a79dc9c82d
Darren Govoni <darren@rapidquest.ai>
2022-02-28 19:55:52 -0500

    Added ALCF cluster pipeline example

--- blazer/examples/example9.py
+++ blazer/examples/example9.py
@@ -2,9 +2,8 @@ from blazer.hpc.alcf import cooley, thetagpu
 
 job1 = cooley.job(n=2, q="debug", A="datascience", password=True, script="/home/dgovoni/git/blazer/testcooley.sh")            
 job2 = thetagpu.job(n=1, q="single-gpu", A="datascience", password=True, script="/home/dgovoni/git/blazer/testthetagpu.sh")
-job3 = thetagpu.job(n=1, q="single-gpu", A="datascience", password=True, script="/home/dgovoni/git/blazer/testthetagpu.sh")
 
-result = list("data" | job1 | job2 | job3)
+result = list("start" | job1 | job2 )
 print(result)
 
 '''     
