f7eec2de875bc8ec7330d760dd04211b763e3f0b
Darren Govoni <darren@rapidquest.ai>
2022-03-04 12:34:05 -0500

    GPU scheduler update

--- blazer/hpc/mpi/primitives.py
+++ blazer/hpc/mpi/primitives.py
@@ -61,7 +61,7 @@ class begin:
                 logging.debug("[%s][%s] Sending break to master",host,rank)
                 comm.send("break", dest=0, tag=1)  
                 logging.debug("[%s][%s] Waiting on barrier",host,rank)
-                comm.Barrier()
+                #comm.Barrier()
                 # TODO: This line seems to work or break depending on mpi implementation
 
                 logging.debug("[%s][%s] Past barrier",host,rank)
