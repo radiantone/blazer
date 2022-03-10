fa124ecdcd17ba6f3711eec6e4f4f74e0b27427b
Darren Govoni <darren@rapidquest.ai>
2022-03-10 12:28:47 -0500

    Updates

--- blazer/examples/example4.py
+++ blazer/examples/example4.py
@@ -1,6 +1,6 @@
 import logging
 logging.basicConfig(
-    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.DEBUG
+    format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
 )
 import blazer
 import numpy as np
