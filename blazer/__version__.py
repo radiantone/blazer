22c390f0a427416ce5d5d9ef4b1f0bb6b7c33fa6
Darren Govoni <darren@rapidquest.ai>
2022-03-15 19:47:36 -0400

    Update __version__.py

--- blazer/__version__.py
+++ blazer/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the py-package-template package.
 
 __title__ = "blazer"
 __description__ = "An HPC abstraction over MPI that uses pipes and pydash primitives for composable super-computing."
-__version__ = 0.1.28
+__version__ = "0.1.28"
 __author__ = "Darren Govoni"
 __author_email__ = "darren@ontrenet.com"
 __license__ = "CC0-1.0 License"
