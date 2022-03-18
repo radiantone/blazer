ba8a4844796738e23b82d6e8c371276cb9575eb7
Darren Govoni <darren@rapidquest.ai>
2022-03-18 05:33:15 -0400

    Updates

--- blazer/__init__.py
+++ blazer/__init__.py
@@ -2,9 +2,9 @@ import logging
 
 from .hpc.gpu import gpu
 from .hpc.mpi.primitives import MASTER as ROOT
-from .hpc.mpi.primitives import begin
+from .hpc.mpi.primitives import begin, environment
 from .hpc.mpi.primitives import mprint as print
-from .hpc.mpi.primitives import stop, environment
+from .hpc.mpi.primitives import stop
 
 logging.basicConfig(
     format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
