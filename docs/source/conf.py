5cdf435d8984c4203e4a50be45424b061d3e34c0
Darren Govoni <darren@rapidquest.ai>
2022-03-18 09:44:50 -0400

    Hot fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Darren Govoni'
 author = 'Darren Govoni'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.34'
+release = '0.1.35'
 
 
 # -- General configuration ---------------------------------------------------
